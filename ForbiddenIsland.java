1d504ca3e07fade2c5e2b91bca57cc3aee227a5d
obermiller.k <obermiller.k@husky.neu.edu>
2016-03-26 00:03:10 -0400

    Created ocean cell
    Created the OceanCell class and corresponding methods.

--- ForbiddenIsland.java
+++ ForbiddenIsland.java
@@ -3,7 +3,7 @@ import javalib.worldimages.*;
 import java.awt.Color;
 import java.util.ArrayList;
 
-// Represents a cell.
+// A cell of land.
 class Cell {
     static final int CELL_SIZE = 20;
     
@@ -45,5 +45,35 @@ class Cell {
                     new Color(0x00FF00));
         }
         return new OverlayImage(cell, background);
+    }
+}
+
+
+// A cell in the ocean.
+class OceanCell extends Cell {
+    OceanCell(int x, int y) {
+        super(0, x, y);
+        this.isFlooded = true;
+    }
+    
+    @Override
+    // EFFECT: Sets the flooded state of this ocean cell, which is always true.
+    void setFlooded(boolean isFlooded) {}
+    
+    @Override
+    // compares the height of this ocean cell to the height of that cell.
+    //  Returns: the height of that cell, which is
+    //           0  if height of that is even with ocean level.
+    //           <0 if height of that is below ocean level.
+    //           >0 if height of that is above ocean level.
+    double compareHeight(Cell that) {
+        return that.height;
+    }
+    
+    @Override
+    // Draws this ocean cell onto the given background.
+    WorldImage drawOnto(WorldImage background) {
+        return new OverlayImage(new RectangleImage(Cell.CELL_SIZE, Cell.CELL_SIZE,
+                OutlineMode.SOLID, Color.BLUE), background);
     }
 }
