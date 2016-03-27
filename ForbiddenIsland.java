68de61ac723dd2816c9b46a1247de9d6a9cb793c
obermiller.k <obermiller.k@husky.neu.edu>
2016-03-27 17:33:59 -0400

    Removed extraneous files
    Deleted non-combined files, renamed combined file to
    ForbiddenIsland.java

--- ForbiddenIsland.java
+++ ForbiddenIsland.java
@@ -1,7 +1,8 @@
-import javalib.impworld.*;
-import javalib.worldimages.*;
-import java.awt.Color;
-import java.util.ArrayList;
+import java.util.ArrayList; 
+import tester.*; 
+import javalib.impworld.*; 
+import java.awt.Color; 
+import javalib.worldimages.*; 
 
 // A cell of land.
 class Cell {
@@ -91,22 +92,23 @@ class Board {
     
     void makeMountainBoard() {
         ArrayList<ArrayList<Double>> hieghts = new ArrayList<ArrayList<Double>>();
-        double middle = ForbiddenIsland.ISLAND_SIZE / 2.0;
+        double middle = ForbiddenIslandWorld.ISLAND_SIZE / 2.0;
         
-        for (int i = 0; i < ForbiddenIsland.ISLAND_SIZE; i++) {
+        for (int i = 0; i < ForbiddenIslandWorld.ISLAND_SIZE; i++) {
             hieghts.add(new ArrayList<Double>());
-            for (int j = 0; j < ForbiddenIsland.ISLAND_SIZE; j++) {
+            for (int j = 0; j < ForbiddenIslandWorld.ISLAND_SIZE; j++) {
                 
             }
         }
     }
     
-    ArrayList<ArrayList<Cell>> doubleListToCellList(ArrayList<ArrayList<Double>> heights) {
+    ArrayList<ArrayList<Cell>> 
+    doubleListToCellList(ArrayList<ArrayList<Double>> heights) {
         ArrayList<ArrayList<Cell>> result = new ArrayList<ArrayList<Cell>>();
         
-        for (int i = 0; i < ForbiddenIsland.ISLAND_SIZE; i++) {
+        for (int i = 0; i < ForbiddenIslandWorld.ISLAND_SIZE; i++) {
             result.add(new ArrayList<Cell>());
-            for (int j = 0; j < ForbiddenIsland.ISLAND_SIZE; j++) {
+            for (int j = 0; j < ForbiddenIslandWorld.ISLAND_SIZE; j++) {
                 result.get(i).add(new Cell(heights.get(i).get(j), i, j));
             }
         }
@@ -123,7 +125,143 @@ class Board {
     }
 }
 
+class Player
+{
+    int x, y;
+    int steps; // number of steps player has taken
+    Board board;
 
-class ForbiddenIsland {
-    static final int ISLAND_SIZE = 64;
+    boolean canMove(String move)
+    {
+        switch (move)
+        {
+            case "up":
+                return board.getCell(x, y + 1) != null;
+                break;
+            case "down":
+                return board.getCell(x, y - 1) != null;
+                break;
+            case "left":
+                return board.getCell(x - 1, y) != null;
+                break;
+            case "right":
+                return board.getCell(x + 1, y) != null;
+                break;
+        }   
+    }
+
+    void move(String move)
+    {
+        if (canMove(move))
+        {
+            switch (move)
+            {
+                case "up":
+                    y += 1;
+                    break;
+                case "down":
+                    y -= 1;
+                    break;
+                case "left":
+                    x -= 1;
+                    break;
+                case "right":
+                    x += 1;
+                    break;
+                default:
+                    break;
+            }   
+        }
+    }
+
+    WorldImage drawOnto()
+    {
+        return new FromFileImage("pilot-icon.png");
+    }
 }
+
+class ForbiddenIslandWorld extends World
+{
+    ArrayList<ArrayList<Cell>> board; // all the cells
+    ArrayList<ArrayList<Double>> heights; // a record of all the cell's heights
+    ArrayList<String> undo1;
+    int waterHeight; // the height of the water
+    static final int ISLAND_SIZE = 64; // constant val
+    static final int BACKGROUND_SIZE = Cell.CELL_SIZE * ISLAND_SIZE;
+    Player player1;
+    final int waterIncrease;
+
+    ForbiddenIslandWorld()
+    {
+        // defaults as mountain island
+        // probably some code from the board class with the mountain
+    }
+
+
+    public void onKeyEvent(String key)
+    {
+        switch(key)
+        {
+            case "up":
+            case "down":
+            case "left":
+            case "right":
+                player1.move(key);
+                undo1.add(key);
+                break;
+            case "u": // undo
+                player1.move(this.oppositeDirection(undo1.get(undo1.size())));
+                undo1.remove(undo1.size());
+                break;
+            case "r": // random island
+                board.makeRandomBoard();
+                break;
+            case "d": // diagonal island
+                board.makeDiamondBoard();
+                break;
+        }
+    }
+    
+    String oppositeDirection(String dir)
+    {
+        switch (dir)
+        {
+            case "up":
+                return "down";
+            case "down":
+                return "up";
+            case "left":
+                return "right";
+            case "right":
+                return "left";
+            default:
+                return "";
+        }
+    }
+
+    public void onTick()
+    {
+        waterHeight += waterIncrease;
+        
+        board.updateBoard();
+    }
+
+    public WorldScene makeScene()
+    {
+        return this.getEmptyScene()
+                .placeImageXY(this.player1.drawOnto(), this.player1.x, this.player1.y)
+                .placeImageXY(this.board.drawOnto(), this.board.x, this.board.y);
+    }
+}
+
+
+class ExampleIsland 
+{
+
+    public static void main(String[] args)
+    {
+        ForbiddenIslandWorld game = new ForbiddenIslandWorld();
+        game.bigBang(w, h);
+    }
+}
+
